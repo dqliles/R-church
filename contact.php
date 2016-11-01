cddfdd85c0dd10a8614895a64d1e091f12e390e3
Darrell <dqliles@yahoo.com>
2016-11-01 17:50:12 -0400

    c

---
+++ contact.php
@@ -0,0 +1,340 @@
+<!DOCTYPE HTML>
+<html class="no-js">
+<head>
+<!-- Basic Page Needs
+  ================================================== -->
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<title>R Church</title>
+<meta name="description" content="">
+<meta name="keywords" content="">
+<meta name="author" content="">
+<!-- Mobile Specific Metas
+  ================================================== -->
+<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0">
+<meta name="format-detection" content="telephone=no">
+<!-- CSS
+  ================================================== -->
+<link href="css/bootstrap.css" rel="stylesheet" type="text/css">
+<link href="css/bootstrap-theme.css" rel="stylesheet" type="text/css">
+<link href="css/style.css" rel="stylesheet" type="text/css">
+<link href="vendor/prettyphoto/css/prettyPhoto.css" rel="stylesheet" type="text/css">
+<link href="vendor/mediaelement/mediaelementplayer.css" rel="stylesheet" type="text/css">
+<link href='vendor/fullcalendar/fullcalendar.css' rel='stylesheet'>
+<link href='vendor/fullcalendar/fullcalendar.print.css' rel='stylesheet' media='print'>
+<!--[if lte IE 9]><link rel="stylesheet" type="text/css" href="css/ie.css" media="screen" /><![endif]-->
+<link href="css/custom.css" rel="stylesheet" type="text/css"><!-- CUSTOM STYLESHEET FOR STYLING -->
+<!-- Color Style -->
+<link href="colors/color1.css" rel="stylesheet" type="text/css">
+<link rel="stylesheet" href="css/mystyle.css" media="screen" type="text/css" />
+    <link href='http://fonts.googleapis.com/css?family=Lato:400,700' rel='stylesheet' type='text/css'>
+
+<!-- SCRIPTS
+  ================================================== -->
+<script src="js/modernizr.js"></script><!-- Modernizr -->
+</head>
+<body>
+<!--[if lt IE 7]>
+	<p class="chromeframe">You are using an outdated browser. <a href="http://browsehappy.com/">Upgrade your browser today</a> or <a href="http://www.google.com/chromeframe/?redirect=true">install Google Chrome Frame</a> to better experience this site.</p>
+<![endif]-->
+<input type="checkbox" id="sidebartoggler" name="" value="">
+<label for="sidebartoggler"  class="toggle">☰</label>
+
+<div class="wrap">
+
+<div class="page-content"> 
+	<!-- Start Site Header -->
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	<header class="site-header">
+    	
+    	<div class="container for-navi">
+        	<div class="site-logo">
+            <h1>
+                <a href="index.html">
+                   
+                    <span class="logo-text">Adore <span>Church</span></span>
+                  
+                </a>
+            </h1>
+            </div>
+            
+            
+            
+            <!-- Main Navigation -->
+             <nav class="main-navigation" role="navigation">
+                <ul class="sf-menu">
+                   
+                  		
+                    		
+                     		
+                    <li><a href="about.html" >About </a>
+                  		
+                    		
+                    </li>
+                    <li><a href="single-sermon.html">Sermons</a></li>
+                    
+                    <li><a href="staff.html">Staff</a></li>
+                            
+                    <li><a href="events.html">Events</a></li>
+                  		
+               
+                	<li><a href="contact.html">Contact</a></li>
+
+                  		
+                   
+                
+            </nav>
+             <!--<a href="#" class="visible-sm visible-xs" id="menu-toggle"><i class="fa fa-bars"></i></a>-->
+    	</div>
+	</header>
+	<!-- End Site Header -->
+   
+   
+	<!-- End Site Header -->
+    <!-- Start Page Header -->
+        <div class="page-header parallax clearfix" style="background-image:url(./images/bg1.jpg);">
+       
+    </div>
+  
+    	
+    </div>
+    <!-- End Page Header -->
+    <!-- Breadcrumbs -->
+    <div class="lgray-bg breadcrumb-cont">
+    	<div class="container">
+          	<ol class="breadcrumb">
+            	<li><a href="index.html">Home</a></li>
+            	<li class="active">Contact</li>
+          	</ol>
+        </div>
+    </div>
+    <!-- Start Body Content -->
+  	<div class="main" role="main">
+    	<div id="content" class="content full">
+        	<div class="container">
+                <div class="row">
+                	<div class="col-md-4 col-sm-5">
+                    	<h2>Contact Us</h2>
+                    	<hr class="sm">
+                    	<h4 class="short accent-color">Fruitland,Maryland</h4>
+                    	<p>777, path to God<br>1800-989-990<br><a href="mailto:us@adorechurch.com">us@Rchurch.com</a></p>
+                        
+                   	</div>
+                    <div class="col-md-8 col-sm-7">
+                       	  <div class="col-md-12">
+                        
+                        
+                       
+    <form action="https://formspree.io/dqliles@gmail.com"
+      method="POST" class="dark-matter">
+    <h1>Contact Form 
+        <span>Please fill all the texts in the fields.</span>
+    </h1>
+    <label>
+        <span>Your Name :</span>
+        <input id="name" type="text" name="name" placeholder="Your Full Name">
+    </label>
+    
+    <label>
+        <span>Your Email :</span>
+        <input id="email" type="email" name="email" placeholder="Valid Email Address">
+    </label>
+    
+    <label>
+        <span>Message :</span>
+         <textarea name="message" placeholder="Your message"></textarea>
+    </label> 
+    
+     <label>
+        
+          <button type="submit">
+  <img class="splash" src="images/watersplash_3.svg" alt="Splash!" />
+  <img class="splash splash-2" src="images/watersplash_3.svg" alt="Splash!" />
+  <img class="sub" src="images/sublogo_16.svg" class="Submarine" />
+  <img class="submerge" src="images/submerge.svg" alt="Water" />
+  <span class="main-text">
+    <span class="submit">Sub</span>mit
+  </span>
+  <span class="sent-text">
+    <span class="sent">Sent!</span>
+  </span>
+</button>
+</form>
+                        
+                        
+                            
+                        <div class="clearfix"></div>
+                        <div id="message"></div>
+                    </div>
+               	</div>
+           	</div>
+        </div>
+   	</div>
+    <!-- End Body Content -->
+    
+    <!-- Start site footer -->
+    <footer class="site-footer">
+       	<div class="container">
+    		<div class="site-footer-top">
+            	
+        		<!-- Quick Info -->
+        		<div class="quick-info">
+                    <div class="row">
+                        <div class="col-md-3 col-sm-6">
+                            <h4><i class="fa fa-clock-o"></i> Service Times</h4>
+                            <p>Sundays @ 10:00 am<br>Starting October 1</p>
+                        </div>
+                        <div class="col-md-3 col-sm-6">
+                            <h4><i class="fa fa-map-marker"></i> Our Location</h4>
+                            <p>777, Path to God<br>United States of America</p>
+                        </div>
+                        <div class="col-md-3 col-sm-6">
+                            <h4><i class="fa fa-envelope"></i> Contact Info</h4>
+                            <p>11 - 00 - 653240<br>email@Rchurch.com</p>
+                        </div>
+                        <div class="col-md-3 col-sm-6">
+                            <h4><i class="fa fa-clock-o"></i> Socialize with us</h4>
+                            <ul class="social-icons-colored inversed">
+                                <li class="facebook"><a href="https://www.facebook.com/groups/1670801793234898/"><i class="fa fa-facebook"></i></a></li>
+                               
+                                <li class="twitter"><a href="http://twitter.com/"><i class="fa fa-twitter"></i></a></li>
+                              
+                            </ul>
+                        </div>
+                    </div>
+            	</div>
+        		<div class="site-footer-bottom">
+            		<div class="row">
+                		<div class="col-md-6 col-sm-6 copyrights-coll">
+        					&copy; 2016 R Church. All Rights Reserved
+            			</div>
+                		<div class="col-md-6 col-sm-6 copyrights-colr">
+        					<nav class="footer-nav" role="navigation">
+                        		<ul>
+                            		<li><a href="index.html">Home</a></li>
+                            		<li><a href="donate.html">Donate now</a></li>
+                            		<li><a href="new-here.html">New here?</a></li>
+                            		<li><a href="contact.html">Contact</a></li>
+                            	</ul>
+                        	</nav>
+            			</div>
+                   	</div>
+               	</div>
+           	</div>
+        </div>
+    </footer>
+    <!-- End site footer -->
+  	<a id="back-to-top"><i class="fa fa-angle-double-up"></i></a>  
+</div>
+</div>
+<div class="sidebar">
+				
+                    
+                  		
+                    <li><a href="about.html" class="one">About </a>
+                  		
+                    		
+                    </li>
+                    <li><a href="single-sermon.html">Sermons</a></li>
+                    
+                    <li><a href="staff.html">Staff</a></li>
+                            
+                    <li><a href="events.html">Events</a></li>
+                  		
+               
+                	<li><a href="contact.html">Contact</a></li>
+
+</div>
+<script src="js/jquery-2.0.0.min.js"></script> <!-- Jquery Library Call -->
+<script src="vendor/prettyphoto/js/prettyphoto.js"></script> <!-- PrettyPhoto Plugin -->
+<script src="js/helper-plugins.js"></script> <!-- Helper Plugins -->
+<script src="js/bootstrap.js"></script> <!-- UI -->
+<script src="js/init.js"></script> <!-- All Scripts -->
+<script src="js/index.js"></script>
+
+<script src="vendor/flexslider/js/jquery.flexslider.js"></script> <!-- FlexSlider -->
+<script src="vendor/countdown/js/jquery.countdown.min.js"></script> <!-- Jquery Timer -->
+<script src="vendor/mediaelement/mediaelement-and-player.min.js"></script> <!-- MediaElements --> 
+<script src="http://maps.googleapis.com/maps/api/js?sensor=false"></script>
+<script type="text/javascript">
+var geocoder = new google.maps.Geocoder();
+var address = "305 East Main Street 399 Fruitland, Maryland - 21826 USA"; //Add your address here, all on one line.
+var latitude;
+var longitude;
+var color = "#3bafda"; //Set your tint color. Needs to be a hex value.
+
+function getGeocode() {
+	geocoder.geocode( { 'address': address}, function(results, status) {
+		if (status == google.maps.GeocoderStatus.OK) {
+    		latitude = results[0].geometry.location.lat();
+			longitude = results[0].geometry.location.lng(); 
+			initGoogleMap();   
+    	} 
+	});
+}
+
+function initGoogleMap() {
+	var styles = [
+	    {
+	      stylers: [
+	        { saturation: -100 }
+	      ]
+	    }
+	];
+	
+	var options = {
+		mapTypeControlOptions: {
+			mapTypeIds: ['Styled']
+		},
+		center: new google.maps.LatLng(latitude, longitude),
+		zoom: 13,
+		scrollwheel: false,
+		navigationControl: false,
+		mapTypeControl: false,
+		zoomControl: true,
+		disableDefaultUI: true,	
+		mapTypeId: 'Styled'
+	};
+	var div = document.getElementById('contact-map');
+	var map = new google.maps.Map(div, options);
+	marker = new google.maps.Marker({
+	    map:map,
+	    draggable:false,
+	    animation: google.maps.Animation.DROP,
+	    position: new google.maps.LatLng(latitude,longitude)
+	});
+	var styledMapType = new google.maps.StyledMapType(styles, { name: 'Styled' });
+	map.mapTypes.set('Styled', styledMapType);
+	
+	var infowindow = new google.maps.InfoWindow({
+	      content: "<div class='iwContent'>"+address+"</div>"
+	});
+	google.maps.event.addListener(marker, 'click', function() {
+	    infowindow.open(map,marker);
+	  });
+	
+	
+	bounds = new google.maps.LatLngBounds(
+	  new google.maps.LatLng(-84.999999, -179.999999), 
+	  new google.maps.LatLng(84.999999, 179.999999));
+
+	rect = new google.maps.Rectangle({
+	    bounds: bounds,
+	    fillColor: color,
+	    fillOpacity: 0.2,
+	    strokeWeight: 0,
+	    map: map
+	});
+}
+google.maps.event.addDomListener(window, 'load', getGeocode);
+</script>
+</body>
+</html>
